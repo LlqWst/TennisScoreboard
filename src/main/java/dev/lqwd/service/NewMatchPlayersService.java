4371676db8a8f6bb4bf043fdf1d92ee79d772239
Lqwd <AppLoneQW@yandex.ru>
2025-08-09 23:07:14 +0300

    добавил проверки перед на equals name перед созданием ДТО для нового матча.

--- src/main/java/dev/lqwd/service/NewMatchPlayersService.java
+++ src/main/java/dev/lqwd/service/NewMatchPlayersService.java
@@ -4,10 +4,15 @@ import dev.lqwd.dao.PlayerDao;
 import dev.lqwd.dto.match_score.MatchScoreDto;
 import dev.lqwd.dto.NewMatchRequestDto;
 import dev.lqwd.entity.Player;
+import dev.lqwd.exception.BadRequestException;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.Objects;
 
+@Slf4j
 public class NewMatchPlayersService {
 
+    private static final String ERROR_EQUALS_MESSAGE = "Names are equal, Validator couldn't do the job for names: %s, %s";
     private final PlayerDao playerDao = new PlayerDao();
 
     public MatchScoreDto getPLayers(NewMatchRequestDto newMatchRequestDto) {
@@ -28,6 +33,14 @@ public class NewMatchPlayersService {
                                 .name(player2)
                                 .build())
                 );
+
+        if(Objects.equals(Player1.getId(), Player2.getId())){
+
+            log.error("Names are equals: {}, {}", Player1.getName(), Player2.getName());
+            throw new BadRequestException(ERROR_EQUALS_MESSAGE.formatted(Player1.getName(), Player2.getName()));
+
+        }
+
 
         return MatchScoreDto.builder()
                 .player1(Player1)
